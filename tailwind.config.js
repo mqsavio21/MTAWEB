fdb56d9eba07ed7dce1453ed5197e8187c77e98d
Developer <prama13@gmail.com>
2024-11-07 18:13:16 +0700

    Add container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     "./*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '24px',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1440px',
+        },
+      },
+    },
   },
   plugins: [],
 }
